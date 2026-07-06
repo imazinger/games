692ce5648d7cbcde49e2ecf0e5fea34163c5883c
bross <crossroads227@gmail.com>
2026-07-07 00:05:49 +0900

    Google FireBase無料つけました

--- bear-reversi/js/firebase-config.js
+++ bear-reversi/js/firebase-config.js
@@ -2,13 +2,13 @@
 // 手順は README.md の「オンライン対戦のセットアップ」を参照。
 // Firebase Console > プロジェクトの設定 > マイアプリ > SDK の設定と構成 からコピーできる。
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyDxWt1vEjZ7AzIZOpNJHfohJG3d7iX8Xag",
+  authDomain: "bear-reversi.firebaseapp.com",
+  databaseURL: "https://bear-reversi-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "bear-reversi",
+  storageBucket: "bear-reversi.firebasestorage.app",
+  messagingSenderId: "4101990066",
+  appId: "1:4101990066:web:7895471385c0a175094bab"
 };
 
 export function hasFirebaseConfig() {
